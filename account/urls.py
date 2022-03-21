2e4f58e135805cc2f9bffa5972b8feaf6dc51bee
Jackreece Ejini <jackreeceejini@gmail.com>
2022-03-21 13:11:04 -0400

    built login dashboard

--- account/urls.py
+++ account/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     # post views
     # path('login/', views.user_login, name='login'), 
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('',views.dashboard, name='dashboard'),
 ]
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from telnetlib import LOGOUT
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Login stuff
+LOGIN_REDIRECT_URL = 'dashboard'
+
+LOGIN_URL = 'login'
+
+LOGOUT_URL = 'logout'
