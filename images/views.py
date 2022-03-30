7fce5c8578b67a4fde39f91d1b0101c85fe35a76
Jackreece Ejini <jackreeceejini@gmail.com>
2022-03-30 12:24:01 -0400

    added infinite scrolling for images list

--- images/views.py
+++ images/views.py
@@ -1,3 +1,4 @@
+from django.core import paginator
 from django.shortcuts import render, redirect 
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -7,8 +8,37 @@ from images.models import Image
 from .forms import ImageCreateForm 
 from django.shortcuts import get_object_or_404 
 from .models import Image 
+from common.decorators import ajax_required 
+from django.http import HttpResponse 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger 
 
 
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            # If the request is AJAX and the page is out of range
+            # return an empty page
+            return HttpResponse('')
+        # If page is out of range deliver las page of results
+        images = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return render(request, 
+        'images/image/list_ajax.html',
+        {'section':'images','images':images})
+    return render(request,
+    'images/image/list.html',
+    {'section': 'images', 'images':images})
+
+@ajax_required
 @login_required
 @require_POST
 def image_like(request):
