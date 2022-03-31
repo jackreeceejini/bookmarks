a55432fa73eacf4623116e259138a953dc786c4d
Jackreece Ejini <jackreeceejini@gmail.com>
2022-03-31 18:38:50 -0400

    integrated redis for ranking and logging view s

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -165,3 +165,9 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = "Your google secret here" # Google consumer s
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username])
 }
+
+# REDIS
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0 
+
